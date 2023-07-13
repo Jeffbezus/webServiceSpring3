4d8ca9b155f1262f0c77006b75ac8260130276d1
Jeffbezus <jefersonguedesdeoliveira@gmail.com>
2023-07-13 18:24:52 -0300

    OrderStatus Enum

--- src/main/java/com/oliveira/project/entities/Order.java
+++ src/main/java/com/oliveira/project/entities/Order.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.oliveira.project.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -26,6 +26,7 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
     
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -34,10 +35,21 @@ public class Order implements Serializable {
     public Order() {
     }
     
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public Long getId() {
