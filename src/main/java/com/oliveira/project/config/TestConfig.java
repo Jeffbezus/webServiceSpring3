89f94db92478640b7f004b961c20c916d278316e
Jeffbezus <jefersonguedesdeoliveira@gmail.com>
2023-07-18 16:39:27 -0300

    OrderItem

--- src/main/java/com/oliveira/project/config/TestConfig.java
+++ src/main/java/com/oliveira/project/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.oliveira.project.entities.Category;
 import com.oliveira.project.entities.Order;
+import com.oliveira.project.entities.OrderItem;
 import com.oliveira.project.entities.Product;
 import com.oliveira.project.entities.User;
 import com.oliveira.project.enums.OrderStatus;
 import com.oliveira.project.repositories.CategoryRepository;
+import com.oliveira.project.repositories.OrderItemRepository;
 import com.oliveira.project.repositories.OrderRepository;
 import com.oliveira.project.repositories.ProductRepository;
 import com.oliveira.project.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
     
     @Autowired
     private ProductRepository ProductRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -69,6 +74,13 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
 
     }
