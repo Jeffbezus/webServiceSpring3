70f123d6d7d4b02a93071edf5e0bfe3d1fb4d48a
Jeffbezus <jefersonguedesdeoliveira@gmail.com>
2023-07-19 15:44:20 -0300

    User insert

--- src/main/java/com/oliveira/project/entities/Payment.java
+++ src/main/java/com/oliveira/project/entities/Payment.java
@@ -3,6 +3,8 @@ package com.oliveira.project.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,9 +23,11 @@ public class Payment implements Serializable{
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
+
 
     public Payment(){
 
